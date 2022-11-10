668157cc464591cc7e1ee2f1b04f5d4b050f77c5
feney <brettfene@Bretts-Felux.lan>
2022-11-09 20:15:05 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
+import { Component } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: { firstName: 'Brett', lastName: 'Fene' },
+      company: 'Test Co.'
+    };
+  }
+
+
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Hi {this.state.name.firstName} {this.state.name.lastName}, I work at {this.state.company}
+          </p>
+          <button onClick={() => {
+            this.setState((state, props) => {
+              //  first function passed is some function where return an object to then use to shallow merge against state
+              return {
+                name: {
+                  firstName: 'Lydia', lastName: 'Hilton'
+                }
+              }
+            },
+              () => {
+                console.log(this.state)
+              });
+
+            console.log(this.state)
+            console.log(this.state.name.firstName)
+          }}>Change name</button>
+        </header>
+      </div >
+    );
+  }
 }
 
 export default App;
